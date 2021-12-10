1fa5b96fa65eb6c860ead64b849937d601495884
elshanibra <ibrahimlielsen42@gmail.com>
2021-12-10 20:56:25 +0400

    Test

--- src/main/java/az/iktlab/group_j4/java_lessons/lesson_8/warmup/FactorialEx.java
+++ src/main/java/az/iktlab/group_j4/java_lessons/lesson_8/warmup/FactorialEx.java
@@ -3,6 +3,12 @@ package az.iktlab.group_j4.java_lessons.lesson_8.warmup;
 public class FactorialEx {
 
     public static int calcFactorial(int num) {
-        return 0;
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+
+        }
+
+        return fact;
     }
 }
